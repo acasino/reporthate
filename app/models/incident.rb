251c95c61bad8c3936bff826e2d264fce7c165ae
Andrew <andrew.casin@gmail.com>
2021-04-11 01:29:18 -0500

    Validations for Incident

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -2,4 +2,7 @@ class Incident < ApplicationRecord
   belongs_to :victim
   has_many :volunteers
 
+  validates :description, :location, :request_translator, :request_translator, presence: true
+
 end
+
