f1b58eb1a36eaa53b3d3b3ff0e311e01f37fda4d
Andrew <andrew.casin@gmail.com>
2021-04-11 00:24:09 -0500

    Volunteer relational mapping

--- app/models/victim.rb
+++ app/models/victim.rb
@@ -1,4 +1,6 @@
 class Victim < ApplicationRecord
+    has_secure_password
+
     has_many :incidents
     has_many :volunteers, through: :incidents
 
--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,2 +1,7 @@
 class Volunteer < ApplicationRecord
+    has_secure_password
+
+    has_many :incidents
+    has_many :victims, through: :incidents
+
 end
