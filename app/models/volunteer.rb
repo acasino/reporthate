46a6b195e082ba1d31aa25aa9a1eb8561bab650c
Andrew <andrew.casin@gmail.com>
2021-04-11 01:27:05 -0500

    Validations for Volunteer

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -4,4 +4,9 @@ class Volunteer < ApplicationRecord
     has_many :incidents
     has_many :victims, through: :incidents
 
+    validates :name, :email, presence: true
+    validates :email, uniqueness: { case_sensitive: false}
+    
+    validates :password, confirmation: true, on: :create
+
 end
