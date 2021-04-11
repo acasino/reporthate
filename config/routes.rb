b5925db9175fcab767940a7c6e590bb8dd217008
Andrew <andrew.casin@gmail.com>
2021-04-11 01:59:19 -0500

    Add victims resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :incidents
+
   resources :volunteers
-  resources :victims
+
+  resources :victims, controller: 'victims/users', path: 'victim', except: [:index] do 
+    resource :incidents, controller: 'victims/incidents'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
