760e3c3bf4ae0a9c4540daaa54100bfee512444f
Andrew <andrew.casin@gmail.com>
2021-04-11 02:39:29 -0500

    Add new and create methods to Victims

--- app/controllers/victims/users_controller.rb
+++ app/controllers/victims/users_controller.rb
@@ -1,7 +1,17 @@
 class Victims::UsersController < ApplicationController
 
     def new
+        @victim = Victim.new
+    end
 
+    def create
+        @victim = Victim.new(user_params)
+        if @victim.save
+            session[:user_id] = @victim.id
+            redirect_to victim_path(@victim)
+        else
+            render :new
+        end
     end
 
     def index
@@ -9,10 +19,6 @@ class Victims::UsersController < ApplicationController
     end
 
     def show
-
-    end
-
-    def create
 
     end
 
