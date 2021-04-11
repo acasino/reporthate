ca9901c0c58089df58790666a9bb4e6515b978d8
Andrew <andrew.casin@gmail.com>
2021-04-11 01:25:28 -0500

    Validations for Victim

--- app/models/victim.rb
+++ app/models/victim.rb
@@ -4,4 +4,9 @@ class Victim < ApplicationRecord
     has_many :incidents
     has_many :volunteers, through: :incidents
 
+    validates :name, :email, presence: true
+    validates :email, uniqueness: { case_sensitive: false}
+    
+    validates :password, confirmation: true, on: :create
+
 end
