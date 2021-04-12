66b2da85c7f00578ec33f55bc06a6231c3483527
Andrew <andrew.casin@gmail.com>
2021-04-12 00:47:57 -0500

    Update sessions controller to include volunteer

--- app/controllers/sessions/sessions_controller.rb
+++ app/controllers/sessions/sessions_controller.rb
@@ -2,13 +2,13 @@ class Sessions::SessionsController < ApplicationController
 
     def create
         @victim = Victim.find_by(email: params[:email])
-        # @volunteer = Volunteer.find_by(email: params[:email])
+        @volunteer = Volunteer.find_by(email: params[:email])
         if @victim && @victim.authenticate(params[:password])
             session[:user_id] = @victim.id
             redirect_to victim_path(@victim)
-        # elsif @volunteer && @volunteer.authenticate(params[:password])
-            # session[:user_id] = @volunteer.id
-            # redirect_to volunteer_path(@volunteer)
+        elsif @volunteer && @volunteer.authenticate(params[:password])
+            session[:user_id] = @volunteer.id
+            redirect_to volunteer_path(@volunteer)
         else
             flash[:error] = "Incorrect Username or Password"
             redirect_to root_path
