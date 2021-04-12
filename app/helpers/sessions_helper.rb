6136aee21b27581525a76d2a6f4de7b7cc90b93e
Andrew <andrew.casin@gmail.com>
2021-04-12 01:01:35 -0500

    Update Sessions helper methods

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,15 +1,31 @@
 module SessionsHelper
 
     def current_user
-
+        if is_volunteer?
+            @current_user ||= Volunteer.find_by(id: session[:user_id])
+        elsif is_victim?
+            @current_user ||= Victim.find_by(id: session[:user_id])
+        end
     end
 
     def logged_in?
-
+        !!current_user
     end
 
-    def logout
+    def logout!
+        session.clear
+    end
 
+    def which_user?
+        session[:user_type]
+    end
+
+    def is_volunteer?
+        which_user? == "volunteer"
+    end
+
+    def is_victim?
+        which_user? == "victim"
     end
 
 end
