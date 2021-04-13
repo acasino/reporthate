d2668c07c4b4ece813362569f1891bc4b2dfa180
Andrew <andrew.casin@gmail.com>
2021-04-13 00:40:01 -0500

    Add VictimsHelper

--- app/helpers/victims_helper.rb
+++ app/helpers/victims_helper.rb
@@ -1,2 +1,13 @@
 module VictimsHelper
+
+    def require_victim_login
+        if !logged_in?
+            flash[:error] = "Login required for access"
+            redirect_to login_path
+        elsif !is_victim?
+            flash[:error] = "You must be registered as a victim to access"
+            redirect_to login_path
+        end
+    end
+
 end
