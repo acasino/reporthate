ac650d79fbc77f013223f6cfb2255fd8216e98a0
Andrew <andrew.casin@gmail.com>
2021-04-13 01:10:49 -0500

    Create incidents new method

--- app/controllers/victims/incidents_controller.rb
+++ app/controllers/victims/incidents_controller.rb
@@ -2,7 +2,12 @@ class Victims::IncidentsController < ApplicationController
     include VictimsHelper
 
     def new
-
+        if params[:victim_id] && @victim.incidents.exists?
+            redirect_to victim_path(@victim), flash: { error: "You've already created this incident"}
+        else
+            @incident = Incident.new()
+            @incidents = @victim.incidents
+        end
     end
 
     def create
@@ -32,7 +37,7 @@ class Victims::IncidentsController < ApplicationController
     end
 
     def verify_user
-        
+
     end
 
 end
