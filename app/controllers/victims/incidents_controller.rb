d260edf687ef8aa6875e4501273a44a7fa53ae5a
Andrew <andrew.casin@gmail.com>
2021-04-13 00:42:00 -0500

    Add Victims Incidents controller

--- app/controllers/victims/incidents_controller.rb
+++ app/controllers/victims/incidents_controller.rb
@@ -1,2 +1,38 @@
 class Victims::IncidentsController < ApplicationController
+    include VictimsHelper
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def incident_params
+
+    end
+
+    def verify_user
+        
+    end
+
 end
