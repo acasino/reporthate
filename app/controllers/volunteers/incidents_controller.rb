1958493ae8a94bf159889e4dd4254ef662d1cf48
Andrew <andrew.casin@gmail.com>
2021-04-13 00:54:58 -0500

    Add VolunteersHelper for login and incidents controller

--- app/controllers/volunteers/incidents_controller.rb
+++ app/controllers/volunteers/incidents_controller.rb
@@ -1,2 +1,37 @@
 class Volunteers::IncidentsController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def incident_params
+
+    end
+
+    def verify_user
+        
+    end
+
 end
--- app/helpers/volunteers_helper.rb
+++ app/helpers/volunteers_helper.rb
@@ -1,2 +1,13 @@
 module VolunteersHelper
+
+    def require_volunteer_login
+        if !logged_in?
+            flash[:error] = "Login required for access"
+            redirect_to login_path
+        elsif !is_volunteer?
+            flash[:error] = "You must be a registered Volunteer to access"
+            redirect_to login_path
+        end
+    end
+
 end
