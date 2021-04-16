3e345f94d438271711325d31ddba8f5eaad8cc56
Andrew <andrew.casin@gmail.com>
2021-04-16 02:33:58 -0500

    Create set_incident method, update view

--- app/controllers/incidents/incidents_controller.rb
+++ app/controllers/incidents/incidents_controller.rb
@@ -1,11 +1,18 @@
 class Incidents::IncidentsController < ApplicationController
 
+    before_action :set_incident, only: [:show]
+
     def index
         @incidents = Incident.all
     end
 
     def show
+    end
 
+    private
+
+    def set_incident
+        @incident = Incident.find_by_id(params[:id])
     end
 
 end
