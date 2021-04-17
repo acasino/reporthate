34c853fa7d755e34622ea7b9ff31cfde76ab1089
Andrew <andrew.casin@gmail.com>
2021-04-17 05:32:59 -0500

    Allow only volunteers to access incidents index

--- app/controllers/incidents/incidents_controller.rb
+++ app/controllers/incidents/incidents_controller.rb
@@ -1,5 +1,8 @@
 class Incidents::IncidentsController < ApplicationController
+    include IncidentsHelper
 
+    # before_action :set_user
+    before_action :require_volunteer_login
     before_action :set_incident, only: [:show]
 
     def index
@@ -7,6 +10,11 @@ class Incidents::IncidentsController < ApplicationController
     end
 
     def show
+        # if which_user? == "victim"
+        #     @victim = Victim.find_by(id: params[:id])
+        # elsif which_user? == "volunteer"
+        #     @volunteer = Volunteer.find_by(id: params[:id])
+        # end
     end
 
     private
@@ -15,4 +23,42 @@ class Incidents::IncidentsController < ApplicationController
         @incident = Incident.find_by_id(params[:id])
     end
 
+    # def set_user
+    #     #setup if/else after session built for current_user
+    #     if current_user != Victim.find_by(id: params[:id])
+    #         flash[:error] = "Oops! Something went wrong"
+    #         redirect_to login_path
+    #     else
+    #         @victim = Victim.find_by(id: params[:id])
+    #     end
+    # end
+
+    # def set_user
+    #     byebug
+    #     if session[:user_type] = 'volunteer'
+    #         @volunteer = Volunteer.find_by(id: params[:id])
+    #     elsif session[:user_type] = 'victim'
+    #         @victim = Victim.find_by(id: params[:id])
+    #     end
+    #     byebug
+    # end
+
+    # def set_user
+    #     byebug
+    #     if session[:user_type] = 'volunteer'
+    #         @volunteer = Volunteer.find_by(id: current_user[:id])
+    #     elsif session[:user_type] = 'victim'
+    #         @victim = Victim.find_by(id: current_user[:id])
+    #     end
+    #     byebug
+    # end
+
+    # def set_user
+    #     if session[:user_type] = 'volunteer'
+    #         @volunteer = Volunteer.find_by(id: current_user[:id])
+    #     elsif session[:user_type] = 'victim'
+    #         @victim = Victim.find_by(id: current_user[:id])
+    #     end
+    # end
 end
+
--- app/helpers/incidents_helper.rb
+++ app/helpers/incidents_helper.rb
@@ -1,3 +1,13 @@
 module IncidentsHelper
 
+    def require_volunteer_login
+        if !logged_in?
+          flash[:error] = "You must be logged in to access this section"
+          redirect_to login_path
+        elsif !is_volunteer?
+          flash[:error] = "You must be registered as a volunteer to access this section"
+          redirect_to victim_path(current_user.id)
+        end
+    end
+
 end
