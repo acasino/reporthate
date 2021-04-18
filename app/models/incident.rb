adbf344efb8e529be686ce494f1e764f7e795e90
Andrew <andrew.casin@gmail.com>
2021-04-18 00:21:55 -0500

    Add scope methods to incidents

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -4,5 +4,11 @@ class Incident < ApplicationRecord
 
   validates :description, :location, :request_translator, :request_translator, presence: true
 
+  scope :filter_by_status, -> (status){ where status: "Not Yet Contacted" }
+
+  def volunteered_count
+    Incident.all.where(volunteer_id: self.id).count
+  end
+
 end
 
